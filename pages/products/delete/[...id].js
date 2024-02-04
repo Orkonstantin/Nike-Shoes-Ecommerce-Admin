35a1a9529facd64da693eebd7d47375c2273d6db
or konstantin <104775108+Orkonstantin@users.noreply.github.com>
2024-02-04 22:05:30 +0200

    Fixed delete/[..id].js file

--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -29,7 +29,7 @@ export default function DeleteProductPage() {
   return (
     <Layout>
       <h1 className={"text-center"}>
-        Do you really want to delete &nbsp;"{productInfo?.title}"?
+        Do you really want to delete &quot;{productInfo?.title}&quot;?
       </h1>
       <div className={"flex gap-2 justify-center"}>
         <button className={"btn-red"} onClick={deleteProduct}>
